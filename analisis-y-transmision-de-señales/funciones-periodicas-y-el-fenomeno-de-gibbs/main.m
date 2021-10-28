7e9624aff83a0c03712e156bd772e029bf27a528
Ricardo Andres Calvo Mendez <rcalvom@unal.edu.co>
2021-10-27 21:01:22 -0500

    Avances

--- analisis-y-transmision-de-señales/funciones-periodicas-y-el-fenomeno-de-gibbs/main.m
+++ analisis-y-transmision-de-señales/funciones-periodicas-y-el-fenomeno-de-gibbs/main.m
@@ -1,6 +1,6 @@
-syms t, n;
+syms t n k x(t);
 
-x = inline(input(newline + "Ingresa una función periodica x(t): "))
+x(t) = input(newline + "Ingresa una función periodica x(t): ")
 
 T_0 = input(newline + "Ingresa el valor del periodo fundamental (T_0) de la función x(t): ")
 
@@ -13,11 +13,8 @@ w_0 = 2*pi/T_0
 a(n) = 2/T_0*int(x(t)*cos(n*w_0*t), t, [0 T_0])
 b(n) = 2/T_0*int(x(t)*sin(n*w_0*t), t, [0 T_0])
 
-x_n(t) = a(0)/2;
+x_n(t) = a(0)/2 + symsum(a(k)*cos(k*w_0*t) + b(k)*sin(k*w_0*t), k, 1, N);
 
 
-for k = 1 : N;
-    r = x_n(t) + a(k)*cos(k*w_0*t) + b(k)*sin(k*w_0*t)
-end
 
 x_n(t)
