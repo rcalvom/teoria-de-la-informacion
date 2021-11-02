58abd328787c668827696e954457fb7fb265c003
Nicholson Ochoa <50887006+nsochoaga@users.noreply.github.com>
2021-11-02 16:22:27 -0500

    Update main.m
    Graficación de funciones

--- analisis-y-transmision-de-señales/funciones-periodicas-y-el-fenomeno-de-gibbs/main.m
+++ analisis-y-transmision-de-señales/funciones-periodicas-y-el-fenomeno-de-gibbs/main.m
@@ -11,8 +11,24 @@ T_0 = input(newline + "Ingresa el valor del periodo fundamental (T_0) de la func
 N = input(newline + "Ingresa un vector con los valores de N para truncar la serie de Furier: ");
 
 % TODO: Graficar x(t)
+tiempo=0:0.001:T_0; % Vector de tiempo
+Y_x=x(tiempo);      % función evaluada
+
+%Grafica de la función x(t)
+tiledlayout(ceil(sqrt(size(N,1))),floor(sqrt(size(N,1)))+1) %tamaño de la cuadricula de graficas
+nexttile
+
+%Grafica de x(t)
+grafica=plot(tiempo,Y_x, '-', 'LineWidth',2); title(strcat('función X(t) = ',char(x))); xlabel('t'); ylabel(char(x)); hold on; axis([0 T_0 -1.5 1.5 ]);
+
 for i = 1 : size(N, 1);
-    x_n(t) = truncated_trigonometric_fourier_series(x, T_0, N(i));    
+    x_n(t) = truncated_trigonometric_fourier_series(x, T_0, N(i));  
+    Y_xn=x_n(tiempo);      % función x_n evaluada
+    %grafica con N_i
+    nexttile
+    plot(tiempo,Y_x, '-', 'LineWidth',2); title(strcat('function truncated at N = ',string(N(i))));  xlabel('t'); ylabel(char(x)); hold on;
+    plot(tiempo,Y_xn, '-', 'LineWidth',2);  legend({'x', 'X_n'},'Location','southwest');legend('boxoff');
+    axis([0 T_0 -1.5 1.5 ])
 end
 
 % Se informa al usuario que el proceso de graficado a terminado.
