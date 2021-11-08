fc171d059c3a87dc4d65ba7e5f1125fd8a001c03
Nicholson Ochoa <nsochoaga@unal.edu.co>
2021-11-08 16:56:38 -0500

    Función rectangular

--- analisis-y-transmision-de-señales/transformada-rapida-de-furier-y-filtros/main.m
+++ analisis-y-transmision-de-señales/transformada-rapida-de-furier-y-filtros/main.m
@@ -1,2 +1,21 @@
-syms a t
+function main()
+
+% Valores iniciales
+disp('Ingrese a');
+a = input('a =');
+F = 100;
+Fs=1/F;
+t=[-pi:Fs:pi]; % Vector de tiempo
+f = t/Fs; % Vector de frecuencia
+fr=@(t) (t>-a)&(t<a);
+
+% Pulso rectangular en el dominio especifico
+x=fr(t);
+subplot(2,1,1);
+plot (t, x, 'r'); grid; axis([-2.5 2.5 -0.1 1.1])
+title(strcat('Pulso rectangular entre -', num2str(a),' y ',num2str(a)));
+xlabel('Tiempo(t)');
+ylabel('x(t)')
+
+% Pulso rectangular con FFT
 
