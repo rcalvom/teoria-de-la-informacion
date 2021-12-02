407e9fb1f6a8f31e87cc6c17887600ec516fd03d
Nicholson Ochoa <50887006+nsochoaga@users.noreply.github.com>
2021-12-02 14:45:18 -0500

    Formatos de señalización

--- transmision-de-senales-analogicas/main.m
+++ transmision-de-senales-analogicas/main.m
@@ -65,6 +65,141 @@ disp(train)
 
 %%% D. Gráfica de la codificación %%%
 
+arr
+while 1
+    disp("Seleccione el tipo de codificación :");
+    disp("0-Continuar");
+    disp("1-Unipolar NRZ");
+    disp("2-Bipolar NRZ");
+    disp("3-Unipolar RZ");
+    disp("4-Bipolar RZ");
+    disp("5-AMI");
+    disp("6-Manchester");
+    disp("")
+    opcion=input("Ingrese la opción: ");
+    y=[];
+    
+    %Tipos dependiendo de la eleccion
+    f_s=100;
+    %No Return to Zero
+    NRZ=ones(1,f_s);
+    %Return Zero
+    RZ= [ones(1,f_s/2) zeros(1,f_s/2)];
+    %Caso especial para Manchester
+    Man=[ones(1,f_s/2)  -ones(1,f_s/2)] ;
+    
+    switch (opcion)
+        case 0
+            break
+        case 1
+            %El nivel de amplitud se mantiene durante todo el intervalo de bit
+            %Solo admite salidas positivas
+            tipo=NRZ;
+            for i=1:length(arr)
+                switch arr(i)
+                    case 1
+                        %En caso de 1 se grafica el pulso en todo el
+                        %intervalo
+                        y=[y  tipo];
+                    case 0
+                        %En caso de cero no se grafica pulso
+                        y=[y  (0*tipo)];
+                end
+            end
+            titulo="Unipolar NRZ";
+        case 2
+            %El nivel de amplitud se mantiene durante todo el intervalo de bit
+            %Admite salidas positivas y negativas
+            tipo=NRZ;
+            for i=1:length(arr)
+                switch arr(i)
+                    case 1
+                        %En caso de 1 se grafica el pulso positivo en todo
+                        %el intervalo
+                        y=[y  tipo];
+                    case 0
+                        %En caso de 0 se grafica el pulso negativo en todo
+                        %el intervalo
+                        y=[y  -tipo];
+                end
+            end
+            titulo="Bipolar NRZ";
+        case 3
+            %El pulso positivo retorna a cero
+            %Solo admite salidas positivas
+            tipo=RZ;
+            for i=1:length(arr)
+                switch arr(i)
+                    case 1
+                        %En caso de 1 se grafica pulso positivo y a la
+                        %mitad retorna a cero.
+                        y=[y  tipo];
+                    case 0
+                        %En caso de 0 no se grafica pulso
+                        y=[y  (0*tipo)];
+                end
+            end
+            titulo="Unipolar RZ";
+        case 4
+            %Pulsos con retorno a zero en la mitad del bit
+            %Admite salidas positivas y negativas
+            tipo=RZ;
+            for i=1:length(arr)
+                switch arr(i)
+                    case 1
+                        %En caso de 1 se grafica pulso positivo y a la
+                        %mitad retorna a cero.
+                        y=[y  tipo];
+                    case 0
+                        %En caso de 0 se grafica pulso negativo y a la
+                        %mitad retorna a cero.
+                        y=[y  -tipo];
+                end
+            end
+            titulo="Bipolar RZ";
+        case 5
+            %Pulsos de 1 se alternan entre positivo y negativo,pulso 0 ausencia de pulso
+            tipo=RZ;
+            count = 0;
+            for i=1:length(arr)
+                switch arr(i)
+                    case 1
+                        % En caso de 1 alternamos entre par e impar con mod
+                        % para graficar alternadamente.
+                        if mod (count,2) == 0
+                            y=[y  tipo];
+                        else
+                            y=[y  -tipo];
+                        end
+                        count = count + 1;
+                    case 0
+                        %En caso de 0 no se grafica pulso
+                        y=[y  (0*tipo)];
+                end
+            end
+            titulo="AMI RZ";
+        case 6
+            %pulso positivo: inicia positivo termina negativo
+            %pulso negativo: inicia negativo termina positivo
+            tipo=Man;
+            for i=1:length(arr)
+                switch arr(i)
+                    case 1
+                        y=[y  tipo];
+                    case 0
+                        y=[y  -tipo];
+                end
+            end
+            titulo="Manchester";
+    end
+    
+    t1=(0:(length(y)-1))/f_s;
+    figure;
+    plot(t1,y,'LineWidth',3);
+    axis([0 160 -3 3]); title('Señal codificada');xlabel('nT_s'); ylabel('x(nT_s)');title(titulo);
+    xlim([0 50])
+    grid on;
+end
 
 
 %%% E. Demodularización %%%
